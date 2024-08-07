38a2f49a7765a60c507b2a3efcf5903c75569b41
cbjoon <qudwns017@naver.com>
2024-08-07 16:02:23 +0900

    init: 프로젝트 초기 세팅

---
+++ src/test/java/com/team2/finalproject/FinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team2.finalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
