bcf2d26248300f8be66816ffc665c2a996356549
sinsj5858 <goddnjswns58@gmail.com>
2024-08-12 23:03:03 +0900

    fix : 잘못 수정된 코드 재수정

--- src/main/java/com/team2/finalproject/global/entity/BaseEntity.java
+++ src/main/java/com/team2/finalproject/global/entity/BaseEntity.java
@@ -1,20 +1,14 @@
 package com.team2.finalproject.global.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
-import jakarta.persistence.Temporal;
-import jakarta.persistence.TemporalType;
-import java.time.LocalDateTime;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @MappedSuperclass
 @Getter
