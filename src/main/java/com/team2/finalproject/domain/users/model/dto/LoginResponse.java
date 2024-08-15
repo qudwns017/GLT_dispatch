a08ba3f81d62a0f6d546fdc0db74ee7f9b8a0278
myoungjin-kim <kaudwls58@naver.com>
2024-08-15 13:38:47 +0900

    feat: 회원가입, 로그인시 입력 반환 dto 추가
    LoginRequest,
    LoginResponse,
    RegisterAdminRequest,
    RegisterDriverRequest

---
+++ src/main/java/com/team2/finalproject/domain/users/model/dto/LoginResponse.java
@@ -0,0 +1,15 @@
+package com.team2.finalproject.domain.users.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginResponse {
+
+    private String name;
+}
---
+++ src/main/java/com/team2/finalproject/domain/users/model/dto/RegisterDriverRequest.java
@@ -0,0 +1,20 @@
+package com.team2.finalproject.domain.users.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterDriverRequest {
+
+    private long centerId;
+    private long smId;
+    private String name;
+    private String username;
+    private String password;
+    private String phoneNumber;
+}
