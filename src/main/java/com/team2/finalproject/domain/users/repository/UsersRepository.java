2f931131fbba58f73a603f3a0ff3e7a52afeec85
myoungjin-kim <kaudwls58@naver.com>
2024-08-15 13:41:35 +0900

    feat: UserDetailsImpl, UserDetailsServiceImpl 추가

--- src/main/java/com/team2/finalproject/domain/users/repository/UsersRepository.java
+++ src/main/java/com/team2/finalproject/domain/users/repository/UsersRepository.java
@@ -3,5 +3,10 @@ package com.team2.finalproject.domain.users.repository;
 import com.team2.finalproject.domain.users.model.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UsersRepository extends JpaRepository<Users, Long> {
+    Optional<Users> findByUsername(String username);
+    boolean existsByUsername(String username);
+    void deleteByUsername(String username);
 }
---
+++ src/main/java/com/team2/finalproject/global/security/details/UserDetailsServiceImpl.java
@@ -0,0 +1,31 @@
+package com.team2.finalproject.global.security.details;
+
+import com.team2.finalproject.domain.users.exception.UsersErrorCode;
+import com.team2.finalproject.domain.users.exception.UsersException;
+import com.team2.finalproject.domain.users.model.entity.Users;
+import com.team2.finalproject.domain.users.repository.UsersRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+@Slf4j
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UsersRepository usersRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        log.info("사용자 정보 조회: username={}", username);
+        Users users = usersRepository.findByUsername(username)
+                .orElseThrow(() -> new UsersException(UsersErrorCode.NOT_FOUND_USER));
+        return new UserDetailsImpl(users);
+    }
+
+
+
+
+}
