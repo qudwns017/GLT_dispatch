84105597a64f3381a06f2ee48fa6d79f8999e886
myoungjin-kim <kaudwls58@naver.com>
2024-08-15 13:42:49 +0900

    feat: CookieUtil 추가

---
+++ src/main/java/com/team2/finalproject/global/util/cookies/CookieUtil.java
@@ -0,0 +1,45 @@
+package com.team2.finalproject.global.util.cookies;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.ResponseCookie;
+
+import java.time.Duration;
+import java.util.Arrays;
+
+public class CookieUtil {
+    public static void addCookie(HttpServletResponse response, String name, String value, long maxAgeInSeconds) {
+        ResponseCookie cookie = ResponseCookie.from(name, value)
+                .httpOnly(true)
+                .secure(true)
+                .path("/")
+                .maxAge(Duration.ofSeconds(maxAgeInSeconds))
+                .sameSite("None")
+                .build();
+        response.addHeader("Set-Cookie", cookie.toString());
+    }
+
+    public static Cookie getCookie(HttpServletRequest request, String name) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            return Arrays.stream(cookies)
+                    .filter(cookie -> cookie.getName().equals(name))
+                    .findFirst()
+                    .orElse(null);
+        }
+        return null;
+    }
+
+    public static void deleteCookie(HttpServletResponse response, String name) {
+        ResponseCookie cookie = ResponseCookie.from(name, "")
+                .httpOnly(true)
+                .secure(true)
+                .path("/")
+                .maxAge(0)
+                .sameSite("None")
+                .build();
+        response.addHeader("Set-Cookie", cookie.toString());
+    }
+
+}
