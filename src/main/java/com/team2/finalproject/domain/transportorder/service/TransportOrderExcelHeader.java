23170fc9362cb60f62f96039098aebd4007a7359
qudwns017 <qudwns017@naver.com>
2024-08-19 21:27:35 +0900

    feat: 운송 주문 엑셀 양식 헤더 데이터 추가

--- src/main/java/com/team2/finalproject/domain/transportorder/service/TransportOrderExcelHeader.java
+++ src/main/java/com/team2/finalproject/domain/transportorder/service/TransportOrderExcelHeader.java
@@ -2,9 +2,7 @@ package com.team2.finalproject.domain.transportorder.service;
 
 import java.util.ArrayList;
 import java.util.List;
-import lombok.Getter;
 
-@Getter
 public record TransportOrderExcelHeader(String name, String required, String comment, String example) {
 
     public static List<TransportOrderExcelHeader> getTransportOrders() {
@@ -12,77 +10,77 @@ public record TransportOrderExcelHeader(String name, String required, String com
         transportOrders.add(new TransportOrderExcelHeader(
                 "배송유형 (지입/용차/배달)", "필수",
                 "배송유형은 지입/용차/배달로 구분하여 입력해주세요.",
-                "지입"
+                "예시) 지입"
         ));
         transportOrders.add(new TransportOrderExcelHeader(
                 "SM명", "필수",
                 "특정 주문을 작업할 담당 드라이버를 지정해주세요. \nGLT 코리아에 등록되지 않은 드라이버의 이름을 입력하면 오류가 발생합니다.",
-                "이서원"
+                "예시) 이서원"
         ));
         transportOrders.add(new TransportOrderExcelHeader(
                 "운송장번호", "필수",
                 "주문번호에 할당된 운송장번호를 입력해주세요.",
-                "C0029384889"
+                "예시) C0029384889"
         ));
         transportOrders.add(new TransportOrderExcelHeader(
                 "업체주문번호", "선택",
                 "운송장 번호가 할당된 주문번호를 입력해주세요.\n관리의 편리함을 위해 있는 입력칸으로,\n필수값이 아니며 임의로 작성해주셔도 됩니다.",
-                "240812_공동구매"
+                "예시) 240812_공동구매"
         ));
         transportOrders.add(new TransportOrderExcelHeader(
                 "주문유형", "선택",
                 "배송/수거",
-                "수거"
+                "예시) 수거"
         ));
         transportOrders.add(new TransportOrderExcelHeader(
                 "주문접수일", "필수",
                 "주문이 귀사에 접수된 날짜를 양식에 맞게 입력해주세요.\n미래의 날짜는 입력할 수 없습니다.",
-                "YYYYMMDD"
+                "예시) YYYYMMDD"
         ));
         transportOrders.add(new TransportOrderExcelHeader(
                 "작업희망일", "필수",
                 "작업을 희망하는 날짜를 양식에 맞게 입력해주세요.",
-                "YYYYMMDD"
+                "예시) YYYYMMDD"
         ));
         transportOrders.add(new TransportOrderExcelHeader(
                 "희망도착시간", "필수",
                 "희망 도착시간을 양식에 맞게 입력해 주세요.\n24시간제를 기준으로 작성 해주세요.",
-                "15:30"
+                "예시) 15:30"
         ));
         transportOrders.add(new TransportOrderExcelHeader(
                 "고객명", "필수",
                 "고객님의 성함 또는 상호를 입력해주세요.",
-                "홍길동"
+                "예시) 홍길동"
         ));
         transportOrders.add(new TransportOrderExcelHeader(
                 "고객연락처", "필수",
                 "고객님의 연락처를 입력해주세요.",
-                "0212345678, 01000000000"
+                "예시) 0212345678, 01000000000"
         ));
         transportOrders.add(new TransportOrderExcelHeader(
                 "주소", "필수",
                 "도로명 주소와 건물번호를 입력해주세요.\n주소정보누리집(www.juso.go.kr) 양식을 따릅니다.",
-                "테헤란로 11길 22"
+                "예시) 테헤란로 11길 22"
         ));
         transportOrders.add(new TransportOrderExcelHeader(
                 "상세주소", "필수",
                 "상세 주소를 입력해주세요.",
-                "101동 203호"
+                "예시) 101동 203호"
         ));
         transportOrders.add(new TransportOrderExcelHeader(
                 "우편번호", "필수",
                 "배송처의 우편번호를 입력해주세요.\n주소정보누리집(www.juso.go.kr) 양식을 따릅니다.",
-                "06232"
+                "예시) 06232"
         ));
         transportOrders.add(new TransportOrderExcelHeader(
                 "볼륨", "필수",
                 "볼륨을 정수 형태로 입력해주세요.\n*가로 x 세로 x 높이 값을 cm^3을 기준으로 작성해주세요.",
-                "600"
+                "예시) 600"
         ));
         transportOrders.add(new TransportOrderExcelHeader(
                 "중량", "필수",
                 "중량을 정수 형태로 입력해주세요.\n*kg을 기준으로 작성해주세요.",
-                "50"
+                "예시) 50"
         ));
         transportOrders.add(new TransportOrderExcelHeader(
                 "고객전달사항", "선택",
@@ -92,27 +90,27 @@ public record TransportOrderExcelHeader(String name, String required, String com
         transportOrders.add(new TransportOrderExcelHeader(
                 "예상작업시간", "선택",
                 "도착 후 작업 예상 작업 시간(분) 정수 형태로 입력해주세요.\n작업소요시간 셀이 비어있으면 기본값으로 1을 인식합니다.",
-                "30"
+                "예시) 30"
         ));
         transportOrders.add(new TransportOrderExcelHeader(
                 "상품명", "필수",
                 "상품명을 모두 입력해주세요.\n상품이 여러개인 경우 운송장번호를 동일하게 사용하여 다음줄에 입력해주세요.",
-                "스탠리텀블러50"
+                "예시) 스탠리텀블러50"
         ));
         transportOrders.add(new TransportOrderExcelHeader(
                 "상품 코드", "선택",
                 "귀사에서 사용 중인 상품코드를 입력해주세요.",
-                "st05"
+                "예시) st05"
         ));
         transportOrders.add(new TransportOrderExcelHeader(
                 "상품 수량", "필수",
                 "상품의 수량을 정수 형태로 입력해주세요.\n상품수량 셀이 비어있으면 기본값으로 1을 인식합니다.",
-                "5"
+                "예시) 5"
         ));
         transportOrders.add(new TransportOrderExcelHeader(
                 "배차우선순위(긴급/보류)", "선택",
                 "배차우선순위를 입력할 수 있습니다.\n긴급/보류로 구분하여 입력해주세요.",
-                "긴급"
+                "예시) 긴급"
         ));
 
         return transportOrders;
