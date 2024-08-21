ff14aacc6e84892b3021c24ac5241366e1c4c697
myoungjin-kim <kaudwls58@naver.com>
2024-08-21 14:09:20 +0900

    refactor: DispatchNumber 변수 명 변경 및 변수 삭제
    담당자Id 변수명 adminId로 변경 (기존 driverId)
    배차명 dispatchName으로 변경 (기존 dispatchType)
    상차 시작 일시(엑셀 등록시 같이 등록하는) loadStartDateTime 추가

--- src/main/java/com/team2/finalproject/domain/dispatchnumber/model/entity/DispatchNumber.java
+++ src/main/java/com/team2/finalproject/domain/dispatchnumber/model/entity/DispatchNumber.java
@@ -1,6 +1,6 @@
 package com.team2.finalproject.domain.dispatchnumber.model.entity;
 
-import com.team2.finalproject.domain.dispatchnumber.model.type.DispatchNumberStatus;
+import com.team2.finalproject.domain.dispatch.model.type.DispatchStatus;
 import com.team2.finalproject.global.entity.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -8,30 +8,37 @@ import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Getter
 public class DispatchNumber extends BaseEntity {
 
     @Column(nullable = false)
     private Long centerId; // 센터코드
 
     @Column(nullable = false)
-    private Long driverId; // 담당자Id
+    private Long adminId; // 담당자Id
 
     @Column(nullable = false, length = 20)
-    private String dispatchNumber; // 배차번호
+    private String dispatchCode; // 배차번호
 
     @Column(nullable = false, length = 50)
-    private String dispatchType; // 배차명
+    private String dispatchName; // 배차명
+
+    @Column(nullable = false)
+    private LocalDateTime loadStartDateTime; // 상차 시작 일시
 
     @Builder.Default
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
-    private DispatchNumberStatus status = DispatchNumberStatus.WAITING;; // 상태
+    private DispatchStatus status = DispatchStatus.WAITING; // 상태
 
 }
