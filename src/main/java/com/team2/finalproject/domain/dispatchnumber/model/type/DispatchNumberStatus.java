a9dab4eb6111802cbe409991af5edd2e2be4a959
myoungjin-kim <kaudwls58@naver.com>
2024-08-21 14:08:23 +0900

    refactor: DispatchNumberStatus 삭제
    DispatchNumberStatus(삭제)
    -> DispatchStatus로 통일

--- src/main/java/com/team2/finalproject/domain/dispatchnumber/model/type/DispatchNumberStatus.java
+++
@@ -1,7 +0,0 @@
-package com.team2.finalproject.domain.dispatchnumber.model.type;
-
-public enum DispatchNumberStatus {
-    IN_TRANSIT,   // 주행중
-    WAITING,      // 주행대기
-    COMPLETED     // 주행완료
-}
