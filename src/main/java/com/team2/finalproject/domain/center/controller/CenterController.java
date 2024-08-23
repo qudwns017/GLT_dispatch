8284947af0ca7594e9d18f5c406e0758805a7f33
Byeongjun Choi <qudwns017@naver.com>
2024-08-23 23:13:38 +0900

    fix: import 문제 수정

--- src/main/java/com/team2/finalproject/domain/center/controller/CenterController.java
+++ src/main/java/com/team2/finalproject/domain/center/controller/CenterController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.team2.finalproject.domain.center.model.dto.response.CenterResponse;
 import com.team2.finalproject.domain.center.model.dto.request.CenterRequest;
 
 @RestController
