6f632c5624104fedfe2965fe239c9cfd1e24ac1c
qudwns017 <qudwns017@naver.com>
2024-08-24 18:03:02 +0900

    feat: UpdateDeliveryDestinationRequest DTO 추가

---
+++ src/main/java/com/team2/finalproject/domain/deliverydestination/model/dto/request/UpdateDeliveryDestinationRequest.java
@@ -0,0 +1,20 @@
+package com.team2.finalproject.domain.deliverydestination.model.dto.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+
+public record UpdateDeliveryDestinationRequest(
+        @Schema(example = "8톤 윙바디", description = "진입불가 톤 코드")
+        String restrictedTonCode,
+        @Schema(example = "윙바디 진입 불가", description = "비고")
+        String comment,
+        @Schema(example = "70", description = "작업추가 소요시간")
+        Integer delayTime
+) {
+    @Builder
+    public UpdateDeliveryDestinationRequest(String restrictedTonCode, String comment, Integer delayTime) {
+        this.restrictedTonCode = restrictedTonCode;
+        this.comment = comment;
+        this.delayTime = delayTime;
+    }
+}
