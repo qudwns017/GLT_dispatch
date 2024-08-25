145d6b51e0da36d49899b1b02397b8791953e2ca
sinsj5858 <goddnjswns58@gmail.com>
2024-08-25 19:06:14 +0900

    refactor : 코드 분할

--- src/main/java/com/team2/finalproject/domain/dispatchnumber/service/DispatchNumberService.java
+++ src/main/java/com/team2/finalproject/domain/dispatchnumber/service/DispatchNumberService.java
@@ -1,22 +1,20 @@
 package com.team2.finalproject.domain.dispatchnumber.service;
 
 import com.team2.finalproject.domain.center.model.entity.Center;
-import com.team2.finalproject.domain.center.repository.CenterRepository;
 import com.team2.finalproject.domain.deliverydestination.repository.DeliveryDestinationRepository;
 import com.team2.finalproject.domain.dispatch.model.entity.Dispatch;
 import com.team2.finalproject.domain.dispatchdetail.model.entity.DispatchDetail;
 import com.team2.finalproject.domain.dispatchdetail.model.type.DispatchDetailStatus;
 import com.team2.finalproject.domain.dispatchnumber.model.dto.response.DispatchListResponse;
 import com.team2.finalproject.domain.dispatchnumber.model.dto.response.DispatchListResponse.DispatchResponse;
-import com.team2.finalproject.domain.dispatchnumber.model.dto.response.DispatchListResponse.Issue;
 import com.team2.finalproject.domain.dispatchnumber.model.entity.DispatchNumber;
 import com.team2.finalproject.domain.dispatchnumber.repository.DispatchNumberRepository;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
-import org.locationtech.jts.geom.Coordinate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,8 +22,6 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Transactional
 public class DispatchNumberService {
-
-    private final CenterRepository centerRepository;
 
     private final DispatchNumberRepository dispatchNumberRepository;
 
@@ -54,40 +50,57 @@ public class DispatchNumberService {
             .map((dispatch) -> {
                 double progressionRate = (double) dispatch.getDeliveryOrderCount() / dispatch.getCompletedOrderCount() * 100;
 
-                List<DispatchDetail> dispatchDetailList = dispatch.getDispatchDetailList();
-                List<Map<String, Double>> stopoverList = new ArrayList<>();
-                for(DispatchDetail dispatchDetail : dispatchDetailList){
-                    Map<String, Double> stopover = new HashMap<>();
-                    stopover.put("lat", dispatchDetail.getDestinationLatitude());
-                    stopover.put("lon", dispatchDetail.getDestinationLongitude());
-                    stopoverList.add(stopover);
+                List<Map<String, Double>> stopoverList = createStopoverList(dispatch);
+                List<Map<String, Double>> coordinates = createCoordinateList(dispatch);
 
-                    if(dispatchDetail.getDispatchDetailStatus() == DispatchDetailStatus.DELIVERY_DELAY){
-                        String comment = null;
-                        if(dispatchDetail.getDestinationId() !=null){
-                            comment = deliveryDestinationRepository.findByIdWithThrow(dispatchDetail.getDestinationId()).getComment();
-                        }
-                        Issue issue = Issue.of(
-                            dispatchCodeId,
-                            dispatch.getId(),
-                            dispatch.getSmName(),
-                            dispatchDetail.getTransportOrder().getCustomerAddress(),
-                            dispatchDetail.getDestinationId() != null ? dispatchDetail.getDestinationId() : null,
-                            comment);
-                        issueList.add(issue);
-                    }
-                }
-                List<Map<String, Double>> coordinates = new ArrayList<>();
-                for (Coordinate coordinateByPath : dispatch.getPath().getCoordinates()) {
-                    Map<String, Double> coordinate = new HashMap<>();
-                    coordinate.put("lat", coordinateByPath.getY());
-                    coordinate.put("lon", coordinateByPath.getX());
-                    coordinates.add(coordinate);
-                }
+                issueList.addAll(getIssueListOfDispatch(dispatch, dispatchCodeId));
 
                 return DispatchResponse.of(dispatch.getId(), dispatch.getDeliveryStatus().getDescription(), dispatch.getSmName(), dispatch.getCompletedOrderCount(), dispatch.getDeliveryOrderCount(), (int) progressionRate,stopoverList,coordinates );
             }).toList();
 
         return DispatchListResponse.of(dispatchNumber.getDispatchNumber(), dispatchNumber.getDispatchName(),(int) totalProgressionRate,totalCompletedOrderCount,totalDeliveryOrderCount,issueList.size(),startStopover,dispatchResponseList,issueList);
+    }
+
+    private List<Map<String, Double>> createStopoverList(Dispatch dispatch) {
+        return dispatch.getDispatchDetailList().stream()
+            .map(detail -> {
+                Map<String, Double> stopover = new HashMap<>();
+                stopover.put("lat", detail.getDestinationLatitude());
+                stopover.put("lon", detail.getDestinationLongitude());
+                return stopover;
+            }).toList();
+    }
+
+    private List<Map<String, Double>> createCoordinateList(Dispatch dispatch) {
+        return Arrays.stream(dispatch.getPath().getCoordinates())
+            .map(coord -> {
+                Map<String, Double> coordinate = new HashMap<>();
+                coordinate.put("lat", coord.getX());
+                coordinate.put("lon", coord.getY());
+                return coordinate;
+            }).toList();
+    }
+
+    private List<DispatchListResponse.Issue> getIssueListOfDispatch(Dispatch dispatch, Long dispatchCodeId) {
+        List<DispatchListResponse.Issue> issueList = new ArrayList<>();
+
+        for (DispatchDetail dispatchDetail : dispatch.getDispatchDetailList()) {
+            if (dispatchDetail.getDispatchDetailStatus() == DispatchDetailStatus.DELIVERY_DELAY) {
+                String comment = null;
+                if (dispatchDetail.getDestinationId() != null) {
+                    comment = deliveryDestinationRepository.findByIdWithThrow(dispatchDetail.getDestinationId()).getComment();
+                }
+                DispatchListResponse.Issue issue = DispatchListResponse.Issue.of(
+                    dispatchCodeId,
+                    dispatch.getId(),
+                    dispatch.getSmName(),
+                    dispatchDetail.getTransportOrder().getCustomerAddress(),
+                    dispatchDetail.getDestinationId(),
+                    comment
+                );
+                issueList.add(issue);
+            }
+        }
+        return issueList;
     }
 }
