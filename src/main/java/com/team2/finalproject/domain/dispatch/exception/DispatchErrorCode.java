fa92fe1996b4535ef6eada1722709e5650aa3f27
qudwns017 <qudwns017@naver.com>
2024-08-27 17:39:25 +0900

    feat: FindByDispatchId 구현

--- src/main/java/com/team2/finalproject/domain/dispatch/exception/DispatchErrorCode.java
+++ src/main/java/com/team2/finalproject/domain/dispatch/exception/DispatchErrorCode.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum DispatchErrorCode implements ErrorCode {
     WRONG_SEARCH_OPTION(HttpStatus.BAD_REQUEST, "wrong search option"),
+    NOT_FOUND_DISPATCH(HttpStatus.NOT_FOUND, "존재하지 않는 배차입니다."),
     ;
 
     private final HttpStatus httpStatus;
