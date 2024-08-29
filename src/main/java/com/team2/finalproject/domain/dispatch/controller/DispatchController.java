574993bdc133e3643cab9a0fc9f5f9e88b80e428
qudwns017 <qudwns017@naver.com>
2024-08-30 03:11:59 +0900

    feat: 배차 확정 Controller 추가

--- src/main/java/com/team2/finalproject/domain/dispatch/controller/DispatchController.java
+++ src/main/java/com/team2/finalproject/domain/dispatch/controller/DispatchController.java
@@ -1,16 +1,20 @@
 package com.team2.finalproject.domain.dispatch.controller;
 
 import com.team2.finalproject.domain.dispatch.model.dto.request.DispatchCancelRequest;
+import com.team2.finalproject.domain.dispatch.model.dto.request.DispatchConfirmRequest;
 import com.team2.finalproject.domain.dispatch.model.dto.request.DispatchUpdateRequest;
 import com.team2.finalproject.domain.dispatch.model.dto.request.IssueRequest;
 import com.team2.finalproject.domain.dispatch.model.dto.response.DispatchUpdateResponse;
 import com.team2.finalproject.domain.dispatch.service.DispatchService;
+import com.team2.finalproject.global.security.details.UserDetailsImpl;
 import com.team2.finalproject.global.util.response.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +25,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/dispatch")
 public class DispatchController implements SwaggerDispatchController{
     private final DispatchService dispatchService;
+
+    @PostMapping
+    public ResponseEntity<Void> confirmDispatch(@RequestBody DispatchConfirmRequest request, @AuthenticationPrincipal
+                                                UserDetailsImpl user) {
+        dispatchService.confirmDispatch(request, user);
+        return ApiResponse.CREATED();
+    }
 
     @PatchMapping
     public ResponseEntity<Void> cancelDispatch(@RequestBody DispatchCancelRequest request) {
