90011089bc7571c804ad99ae9a933d3425ec28be
sinsj5858 <goddnjswns58@gmail.com>
2024-09-03 22:50:11 +0900

    !HOTFIX : cors설정 header->method로 변경

--- src/main/java/com/team2/finalproject/global/config/CorsConfig.java
+++ src/main/java/com/team2/finalproject/global/config/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowedOrigins(List.of("http://localhost:3000", "https://api.gltkorea.site"));
-        configuration.setAllowedHeaders(List.of("HEAD", "GET", "POST","PUT","DELETE","PATCH","OPTIONS"));
+        configuration.setAllowedMethods(List.of("GET", "POST","PUT","DELETE","PATCH","OPTIONS"));
         configuration.setAllowCredentials(true);
         configuration.addAllowedHeader("*");
 
