8503bfdef44fb82f8220d847a403275cdfc66fc6
sinsj5858 <goddnjswns58@gmail.com>
2024-09-03 23:03:12 +0900

    !HOTFIX : cors hearder설정 변경

--- src/main/java/com/team2/finalproject/global/config/CorsConfig.java
+++ src/main/java/com/team2/finalproject/global/config/CorsConfig.java
@@ -18,7 +18,8 @@ public class CorsConfig {
         configuration.setAllowedOrigins(List.of("http://localhost:3000", "https://api.gltkorea.site"));
         configuration.setAllowedMethods(List.of("GET", "POST","PUT","DELETE","PATCH","OPTIONS"));
         configuration.setAllowCredentials(true);
-        configuration.addAllowedHeader("*");
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setExposedHeaders(List.of("*"));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
