94ca2dac6c344327a035c01e9e1d37d09c30f363
sinsj5858 <goddnjswns58@gmail.com>
2024-09-04 14:35:37 +0900

    !HOTFIX : swagger cors 해결

--- src/main/java/com/team2/finalproject/global/config/SwaggerConfig.java
+++ src/main/java/com/team2/finalproject/global/config/SwaggerConfig.java
@@ -5,13 +5,20 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+
+    @Value("${swagger-ui.request.url}")
+    String requestUrl;
+
     @Bean
     public OpenAPI openAPI() {
+
         String jwt = "JWT";
         SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwt);
         Components components = new Components().addSecuritySchemes(jwt, new SecurityScheme()
@@ -21,10 +28,15 @@ public class SwaggerConfig {
                 .bearerFormat("JWT")
         );
 
+        Server server = new Server()
+            .url(requestUrl)
+            .description("GLT KOREA TMS API Server");
+
         return new OpenAPI()
                 .components(new Components())
                 .info(apiInfo())
                 .addSecurityItem(securityRequirement)
+                .addServersItem(server)
                 .components(components);
     }
     private Info apiInfo() {
