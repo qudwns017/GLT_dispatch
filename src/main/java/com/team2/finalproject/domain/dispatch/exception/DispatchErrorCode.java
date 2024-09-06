c0ea0ef73a491cace0e172f5d38dae5bc5c36fbc
myoungjin-kim <kaudwls58@naver.com>
2024-09-06 16:26:39 +0900

    refactor: 차량 관제 탭, 배차 조회 검색 시작 날짜 LocalDateTime으로 변경

--- src/main/java/com/team2/finalproject/domain/dispatch/exception/DispatchErrorCode.java
+++ src/main/java/com/team2/finalproject/domain/dispatch/exception/DispatchErrorCode.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum DispatchErrorCode implements ErrorCode {
-    WRONG_SEARCH_OPTION(HttpStatus.BAD_REQUEST, "wrong search option"),
+    WRONG_SEARCH_OPTION(HttpStatus.BAD_REQUEST, "올바르지 않은 검색 옵션 입니다."),
     NOT_FOUND_DISPATCH(HttpStatus.NOT_FOUND, "존재하지 않는 배차입니다."),
     ;
 
--- src/main/java/com/team2/finalproject/domain/dispatch/repository/DispatchRepository.java
+++ src/main/java/com/team2/finalproject/domain/dispatch/repository/DispatchRepository.java
@@ -9,21 +9,13 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 public interface DispatchRepository extends JpaRepository<Dispatch, Long> {
 
     // DispatchNumber가 가진 Dispatch 리스트 조회
     @Query("SELECT d FROM Dispatch d WHERE d.dispatchNumber IN :dispatchNumbers")
     List<Dispatch> findByDispatchNumbersIn(@Param("dispatchNumbers") List<DispatchNumber> dispatchNumbers);
-
-    default Map<DispatchNumber, List<Dispatch>> findDispatchMapByDispatchNumbers(List<DispatchNumber> dispatchNumbers) {
-        List<Dispatch> dispatches = findByDispatchNumbersIn(dispatchNumbers);
-        return dispatches.stream()
-                .collect(Collectors.groupingBy(Dispatch::getDispatchNumber));
-    }
 
     default Dispatch findByIdOrThrow(long dispatchId) {
         return findById(dispatchId).orElseThrow(() ->
