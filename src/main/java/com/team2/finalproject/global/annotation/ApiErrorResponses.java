f9dde7d6c8fa74b345142bd24821b1c96af5d4e1
qudwns017 <qudwns017@naver.com>
2024-09-09 21:51:36 +0900

    feat: ApiErrorResponse 어노테이션 추가

---
+++ src/main/java/com/team2/finalproject/global/annotation/ApiErrorResponses.java
@@ -0,0 +1,18 @@
+package com.team2.finalproject.global.annotation;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
+
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({METHOD, TYPE, ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+public @interface ApiErrorResponses {
+    ApiErrorResponse[] value() default {};
+}
+//https://github.com/depromeet/street-drop-server/blob/dev/backend/streetdrop-api/src/main/java/com/depromeet/external/swagger/config/CustomOperationCustomizer.java
